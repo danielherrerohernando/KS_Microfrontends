6cac1c03a13d0f27b817533ae4fdd5b4cd010013
Daniel Herrero <danielherrerohernando@gmail.com>
2019-10-18 13:04:43 +0200

    renaming and restructuring

--- skeleton/components/healthcheck/index.js
+++
@@ -1,6 +0,0 @@
-const System = require('systemic');
-const initHealthCheck = require('./health-check');
-
-module.exports = new System({ name: 'healthcheck' })
-	.add('healthcheck', initHealthCheck());
-// .dependsOn('db', 'bus');
--- skeleton/config/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	logger: { transport: null },
-	metrics: {
-		key: 'this is some key',
-		insightsConfig: {
-			disableAppInsights: true,
-		},
-	},
-};
